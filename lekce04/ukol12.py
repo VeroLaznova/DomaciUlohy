20efa0095b84f693e7569e0e529ee65ffc617e6c
Veronika Laznova <laznova.veronika@gmail.com>
2017-10-23 08:48:49 +0200

    pridavam nove projekty

---
+++ lekce04/ukol12.py
@@ -0,0 +1,5 @@
+for radek in range (1,5):
+    if radek == 1:
+        print('prvni radek')
+    else:
+        print('neni prvni')
---
+++ lekce04/ukol13.py
@@ -0,0 +1,9 @@
+for radek in range (5):
+    for sloupec in range (6):
+        if radek == 0:
+            print ('X ',end='')
+        elif radek == 6:
+            print ('X ',end='')
+
+        else:
+                print ('X ')
